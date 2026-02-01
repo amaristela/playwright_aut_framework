0a4cfcb85d6e7f86fb7cbee6668071fd68d1615e
Mark <markadrianmaristela@Marks-MacBook-Air.local>
2026-02-01 14:45:17 +0800

    refactor: api cases, and prettier format for all files

--- tests/parabank/api/accounts/billpay.spec.ts
+++ tests/parabank/api/accounts/billpay.spec.ts
@@ -1,18 +1,19 @@
-import {expect, request, test} from '@playwright/test';
+import { expect, request, test } from '@playwright/test'
 
-test('API /billpay', async ({request}) => {
-    const response = await request.post('https://parabank.parasoft.com/parabank/services/bank/billpay',
+test('API /billpay', async ({ request }) => {
+    const response = await request.post(
+        'https://parabank.parasoft.com/parabank/services/bank/billpay',
         {
             headers: {
                 'Content-Type': 'application/json',
-                'Accept': 'application/xml',
+                Accept: 'application/xml',
             },
             params: {
                 accountId: '131121',
-                amount: 100
-            }
+                amount: 100,
+            },
         }
-    );
+    )
 
-    expect(response.status()).toBe(200);
+    expect(response.status()).toBe(200)
 })
--- utils/pom/loginPage.ts
+++ utils/pom/loginPage.ts
@@ -1,30 +1,26 @@
-import {expect, type Locator, type Page} from '@playwright/test';
+import { expect, type Locator, type Page } from '@playwright/test'
 
 export class LoginPage {
-    readonly page : Page;
-    readonly username : Locator;
-    readonly password : Locator;
-    readonly btnlogin : Locator;
+    readonly page: Page
+    readonly username: Locator
+    readonly password: Locator
+    readonly btnlogin: Locator
 
-
-    constructor(page:Page) {
-        this.page = page;
-        this.username = page.locator('xpath=//input[@name="username"]');
-        this.password = page.locator('xpath=//input[@name="password"]');
-        this.btnlogin = page.locator('xpath=//input[@value="Log In"]'); 
+    constructor(page: Page) {
+        this.page = page
+        this.username = page.locator('xpath=//input[@name="username"]')
+        this.password = page.locator('xpath=//input[@name="password"]')
+        this.btnlogin = page.locator('xpath=//input[@value="Log In"]')
     }
 
     async gotoUrl() {
-        await this.page.goto('https://parabank.parasoft.com/parabank/index.htm');
-        await expect(this.page).toHaveTitle(/Welcome/);
+        await this.page.goto('https://parabank.parasoft.com/parabank/index.htm')
+        await expect(this.page).toHaveTitle(/Welcome/)
     }
 
-    async login (username: string, password: string) {
-        await this.username.fill(username);
-        await this.password.fill(password);
-        await this.btnlogin.click();
+    async login(username: string, password: string) {
+        await this.username.fill(username)
+        await this.password.fill(password)
+        await this.btnlogin.click()
     }
-
-
 }
-
